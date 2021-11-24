987a5bbb434c7c882fec54abc80f7f45889a0826
SinuheJardinez <sinuhe.jh@gmail.com>
2021-11-24 11:19:13 -0600

    initial coomit

---
+++ controllers/usersController.js
@@ -0,0 +1,4 @@
+
+exports.register = async (req, res)=>{
+    res.send("Estas en registro")
+}
---
+++ index.js
@@ -0,0 +1,26 @@
+//imports : Express para framework, connectDB para conexion a base de datos, app
+const express = require ("express")
+const connectDB = require("./config/db")
+const app = express()
+const hbs = require("hbs")
+
+require("dotenv").config()
+//middlewares
+app.use(express.static("public"))
+
+app.set("views", __dirname+"views")
+app.set("view engine", "hbs")
+
+hbs.registerPartials(__dirname + "/views/partials")
+app.use(express.urlencoded({extended:true}))
+
+connectDB()
+//rutas
+app.use("/users", require("./routes/users"))
+app.use("/", require("./routes/index"))
+
+
+//server
+app.listen(process.env.PORT, ()=>{
+    console.log(`Running server on port ${process.env.PORT}`)
+})
